0fc98dc210353ffb5cfcadc56feadde857522a0c
Abdullah Farooq <afarooq@brickclay.com>
2019-03-27 21:50:35 +0500

    feat: Install Syncfusion file uploader

--- Svg2Font.Client/src/app/app.module.ts
+++ Svg2Font.Client/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { UploaderModule  } from '@syncfusion/ej2-angular-inputs';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, UploaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
